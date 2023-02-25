26f147378ad85e981381908334d58dcd2fe5f0e1
Szymon <120337045+kremowkojad3@users.noreply.github.com>
2023-02-25 21:39:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const formHeading = document.querySelector(".form-heading");
+const formInputs = document.querySelectorAll(".contact-form-input");
+
+formInputs.forEach((input) => {
+  input.addEventListener("focus", () => {
+    formHeading.style.opacity = "0";
+    setTimeout(() => {
+      formHeading.textContent = `Your ${input.placeholder}`;
+      formHeading.style.opacity = "1";
+    }, 300);
+  });
+
+ 
+   
